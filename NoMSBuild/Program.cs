6a6c3d3945ab5d29ee01d7f8ef5f29a62f930e37
rvrsh3ll <rvrsh3ll@users.noreply.github.com>
2020-12-20 21:32:16 -0500

    Initial Commit

---
+++ NoMSBuild/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Evaluation;
+
+namespace NoMSBuild
+{
+
+    public class BuildIt
+    {
+        private static bool BuildProject(string projectPath)
+        {
+            var engine = new ProjectCollection();
+            bool success = engine.LoadProject(projectPath).Build();
+            return success;
+        }
+        static void Main(string[] args)
+        {
+            string projectPath = "buildit.csproj";
+            BuildProject(projectPath);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NoMSBuild
- MSBuild without MSbuild.exe
+MSBuild without MSbuild.exe
+
+## Instructions
+
+Replace the base64 shellcode, process path, and AssemblyFile variable in buildit.csproj to suite your needs.
+
+You may also place buildit.csproj on a remote share (smb/webdav) and modify the projectPath variable in Program.cs of NoMSBuild project to point to it.
